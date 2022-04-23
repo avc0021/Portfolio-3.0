1fce0f0c0e9720aaea702a4a5b4c522ef07ac09a
avc0021 <avc0021@gmail.com>
2022-04-22 22:42:05 -0500

    nav links added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/header.js';
 // import Projects from './components/Projects';
 import About from './components/About/about.js';
 import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
-// import Resume from './components/Resume';
+import Resume from './components/Resume/resume.js';
 // import Contact from './components/Contact';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -15,6 +15,9 @@ function App() {
   const renderPage =() => {    
     if (currentPage === 'About') {
       return <About />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
   }
   const handlePageChange = (page) => setCurrentPage(page);
--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -15,7 +15,7 @@ function Nav() {
       <Flex w="100%">
       <Box w='500px'>
         <Heading
-          fontSize="7xl"
+          fontSize="5xl"
           fontWeight="bold"
           bgGradient="linear(to-r, blue.600, blue.700)"
           bgClip="text"
@@ -33,6 +33,22 @@ function Nav() {
         ></IconButton>
         </Box>
         <Spacer/>
+        <Box w='500px'>
+          <ul>
+            <li>
+              <a href="/"> Home</a>
+            </li>
+            <li>
+              <a href="/#about"> About</a>
+            </li>
+            <li>
+              <a href="/#resume"> Resume</a>
+            </li>
+            <li>
+              <a href="/#resume"> Resume</a>
+            </li>
+          </ul>
+        </Box>
 
         
       </Flex>
