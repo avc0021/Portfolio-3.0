b767843a1de2aa69453c53bd0d704dbaf028cbd6
avc0021 <avc0021@gmail.com>
2022-04-22 20:31:30 -0500

    about is responsive

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav/nav.js';
 import Header from './components/Header/header.js';
 import DarkModeSwitch from './components/DarkModeSwitch/darkmodeswitch.js';
 // import Projects from './components/Projects';
-//import About from './components/About';
+import About from './components/About/about.js';
+import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
 // import Resume from './components/Resume';
 // import Contact from './components/Contact';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Nav />
-      <Header />
-      <DarkModeSwitch/>
 
-    </div>
+  const [currentPage, setCurrentPage] = useState('About');
+  const renderPage =() => {    
+    if (currentPage === 'About') {
+      return <About />;
+    }
+  }
+  const handlePageChange = (page) => setCurrentPage(page);
+  
+  return (
+    <VStack p={5}>
+      <Flex w="100%">
+      <Nav />
+      <DarkModeSwitch/>
+      </Flex>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {renderPage()}
+
+    </VStack>
   );
 }
 
